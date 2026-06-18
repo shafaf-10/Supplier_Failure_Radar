164bb6ce220f6ecd34bc567f5082d39c40d64bf0
shafaf <shafafmusthafa001@gmail.com>
2026-06-18 12:27:31 +0530

    delete_table

--- app/ml/pipeline.py
+++ app/ml/pipeline.py
@@ -1,31 +1,3 @@
-# from app.ml.feature_builder import build_supplier_features
-# from app.ml.anomaly_detector import detect_anomalies
-# def run_prediction_pipeline():
-#     """
-#     Production pipeline:
-#     1. Build latest supplier features from MySQL
-#     2. Predict risk + detect anomalies
-#     3. Update supplier_features and supplier_predictions tables
-#     """
-
-#     print("Starting supplier prediction pipeline...")
-
-#     features_df = build_supplier_features()
-#     detect_anomalies(features_df)
-
-
-#     print("Supplier prediction pipeline completed successfully.")
-
-#     return {
-#         "status": "success",
-#         "message": "Supplier prediction pipeline completed successfully",
-#     }
-
-
-# if __name__ == "__main__":
-#     run_prediction_pipeline()
-
-
 from app.ml.feature_builder import build_supplier_features
 from app.ml.anomaly_detector import detect_anomalies
 
